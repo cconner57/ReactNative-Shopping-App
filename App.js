a126bc9d6d46812e053a436fc998c30afca88122
Chris Conner <43318961+cconner57@users.noreply.github.com>
2020-12-14 13:54:51 -0800

    Finished store app

--- App.js
+++ App.js
@@ -1,103 +1,48 @@
 import React, { useState } from 'react';
-import {
-	StyleSheet,
-	View,
-	Text,
-	TextInput,
-	ScrollView,
-	Button,
-	TouchableOpacity,
-} from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
-import { FontAwesome5 } from '@expo/vector-icons';
-
-import DefaultStyles from './default-styles';
 import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import authReducer from './store/reducers/auth';
+import AppNavigator from './navigation/AppNavigator';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
-	return Font.loadAsync({
-		'grandstander': require('./assets/fonts/Grandstander-Regular.ttf'),
-	});
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
 };
 
 export default function App() {
-	const [newTodo, setNewTodo] = useState('');
-	const [todos, setTodos] = useState(['Orange juice', 'Cheese', 'Carrots']);
-	const [dataLoaded, setDataLoaded] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-	if (!dataLoaded) {
-		return (
-			<AppLoading
-				startAsync={fetchFonts}
-				onFinish={() => setDataLoaded(true)}
-				onError={(err) => console.log(err)}
-			/>
-		);
-	}
-
-	const handleAdd = () => {
-		if (newTodo === '') return;
-		setTodos([...todos, newTodo]);
-		setNewTodo('');
-	};
-
-	const handleDelete = (todoValue) => {
-		setTodos((currentTodos) => {
-			return currentTodos.filter((todo) => todo !== todoValue);
-		});
-	};
-
-	return (
-		<View style={DefaultStyles.Todo}>
-			<Text style={DefaultStyles.Todo__Heading}>Shopping List</Text>
-			<View style={DefaultStyles.Todo__Container}>
-				<View style={DefaultStyles.Todo__Form}>
-					<TextInput
-						style={DefaultStyles.Todo__Input}
-						value={newTodo}
-						placeholder='Enter todo'
-						onChangeText={(text) => setNewTodo(text)}
-					/>
-					<View style={DefaultStyles.Todo__Button}>
-						<Button title='ADD' onPress={handleAdd} color='white' />
-					</View>
-				</View>
-				<View style={DefaultStyles.Todo__List}>
-					<ScrollView>
-						{todos.map((todo, key) => (
-							<TouchableOpacity
-								key={key}
-								activeOpacity={0.6}
-								onPress={() => handleDelete(todo)}>
-								<View style={DefaultStyles.Todo__ItemContainer}>
-									<FontAwesome5
-										name='trash-alt'
-										size={20}
-										color='black'
-										style={DefaultStyles.Todo__Trash}
-									/>
-									<Text style={DefaultStyles.Todo__Item}>{todo}</Text>
-								</View>
-								<View style={DefaultStyles.Todo__DashContainer}>
-									<View style={DefaultStyles.Todo__ItemDash}></View>
-								</View>
-							</TouchableOpacity>
-						))}
-					</ScrollView>
-				</View>
-			</View>
-			<View style={DefaultStyles.Todo__Completed}>
-				<Button color='hsl(175, 50%, 40%)' title='Show Completed' />
-			</View>
-		</View>
-	);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
